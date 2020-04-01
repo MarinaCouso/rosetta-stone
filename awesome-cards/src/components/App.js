fc42d4d94749f211f9ecd4833baf4835cfeda84d
MarinaCouso <marina.couso.martinez@gmail.com>
2020-04-01 18:08:19 +0200

    Footer refactorized except image

--- awesome-cards/src/components/App.js
+++ awesome-cards/src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import logo from '../images/logo.svg';
-import '../stylesheets/App.css';
+import '../stylesheets/App.scss';
 import Landing from './Landing';
 import Card from './Card';
 function App() {
--- awesome-cards/src/components/card/Footer.js
+++ awesome-cards/src/components/card/Footer.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import '../../images/logo-adalab.png';
 function Footer() {
-  return <div></div>;
+  return (
+    <footer className='footer'>
+      <hr className='footer__mark' noshade size='1px' />
+      <small className='footer__copy'>Awesome profile-cards © 2020</small>
+      <div className='footer__logo'>
+        <a href='https://adalab.es/' title='Ir a Adalab' target='_blank'>
+          <img src='../../images/logo-adalab.png' alt='Adalab' />
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
