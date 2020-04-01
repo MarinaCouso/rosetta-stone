04efacb0e0539309609c3736251f32a9ced09580
Martamp <motymont@gmail.com>
2020-04-01 11:50:07 +0200

    first component structure

---
+++ awesome-cards/src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './card/Header';
+import Main from './card/Main';
+import Footer from './card/Footer';
+
+function Card() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default Card;
--- awesome-cards/src/index.js
+++ awesome-cards/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/index.css';
 import App from './components/App';
-import * as serviceWorker from './components/serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
