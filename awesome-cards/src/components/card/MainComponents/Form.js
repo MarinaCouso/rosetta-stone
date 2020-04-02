253e10a2ee086fa1853e90ede17af2d557e645c3
smgaragorri <smgaragorri@gmail.com>
2020-04-02 18:03:42 +0200

    Create the main structure for form inputs

--- awesome-cards/src/components/card/MainComponents/Form.js
+++ awesome-cards/src/components/card/MainComponents/Form.js
@@ -1,7 +1,95 @@
 import React from 'react';
 
+// handleInputData = () => {
+//   if (ev.currentTarget.value !== undefined) {
+//     ev.currentTarget.innerHTML = ev.currentTarget.value;
+//   }
+// };
+
 function Form() {
-  return <div></div>;
+  return (
+    // <div className='section-page__fill collapsable-close'>
+    <form
+      action=''
+      method='GET'
+      id='info-user'
+      className='section-page__fill__form collapsable-content'
+    >
+      <label htmlFor='fullname'>Nombre completo</label>
+      <input
+        className='input-name'
+        type='text'
+        name='fullname'
+        id='fullname'
+        placeholder='Ej: Sally Jill'
+        // onKeyUp={handleInputData}
+        required
+      />
+      <label htmlFor='job'>Puesto</label>
+      <input
+        className='js-input-job'
+        type='text'
+        name='job'
+        id='job'
+        placeholder='Ej: Front-end unicorn'
+        // onKeyUp={handleInputData}
+        required
+      />
+      <label htmlFor='img' class='label-img'>
+        Imagen de perfil
+      </label>
+      <div className='img-container'>
+        <button type='button' class='btn-add js-btn-img'>
+          Añadir imagen
+        </button>
+        <input
+          type='file'
+          name=''
+          id='img-selector'
+          className='hiddenField js__profile-upload-btn'
+          // onChange={handleInputData}
+        />
+        <div className='img-user js-preview'></div>
+      </div>
+      <label htmlFor='email'>Email</label>
+      <input
+        className='js-input-mail'
+        type='email'
+        name='email'
+        id='email'
+        placeholder='Ej: sally-hill@gmail.com'
+        // onKeyUp={handleInputData}
+      />
+      <label htmlFor='phone'>Teléfono</label>
+      <input
+        className='js-input-phone'
+        type='tel'
+        name='phone'
+        id='phone'
+        placeholder='Ej: 555-55-55-55'
+        // onKeyUp={handleInputData}
+      />
+      <label htmlFor='linkendin'>Linkendin</label>
+      <input
+        className='js-input-linkedin'
+        type='text'
+        name='linkendin'
+        id='linkendin'
+        placeholder='Ej: linkendin.com/in/sally.hill@gmail.com'
+        // onKeyUp={handleInputData}
+      />
+      <label htmlFor='github'>Github</label>
+      <input
+        className='js-input-github'
+        type='text'
+        name='github'
+        id='github'
+        placeholder='Ej: @sally-hill'
+        // onKeyUp={handleInputData}
+      />
+    </form>
+    // </div>
+  );
 }
 
 export default Form;
