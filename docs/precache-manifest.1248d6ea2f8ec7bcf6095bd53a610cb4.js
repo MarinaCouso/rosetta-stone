918e5bf25d38cadb8899f77f40b4e693e9098c15
smgaragorri <smgaragorri@gmail.com>
2020-04-07 11:11:30 +0200

    Add information to package.json. Run build. Rename folder build to folder Docs

---
+++ docs/precache-manifest.1248d6ea2f8ec7bcf6095bd53a610cb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0370ee38c979ca18b9070e23dcaba00b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "33d0b4dcf9d125b6d946",
+    "url": "./static/css/main.fa995e00.chunk.css"
+  },
+  {
+    "revision": "3c1ac07cee96d6643d09",
+    "url": "./static/js/2.e4cf8988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e4cf8988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d0b4dcf9d125b6d946",
+    "url": "./static/js/main.601a8b03.chunk.js"
+  },
+  {
+    "revision": "ef0e3c2bbbad658ef01a",
+    "url": "./static/js/runtime-main.4c2e7753.js"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/awesome-profile-cards-logo.0118e311.svg"
+  },
+  {
+    "revision": "034e88c93dc71dc98f92b23fa9b15e24",
+    "url": "./static/media/sample.034e88c9.jpg"
+  }
+]);
