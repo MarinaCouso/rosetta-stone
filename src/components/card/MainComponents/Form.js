8924c9fb2f7288a7fff727c91675e9e78b5acade
Martamp <motymont@gmail.com>
2020-04-08 12:39:53 +0200

    create Image component and first function structure #11

--- src/components/card/MainComponents/Form.js
+++ src/components/card/MainComponents/Form.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SectionTitle from './SectionTitle';
 import FormInput from './FormInput';
+import Image from './Image';
 
 // handleInputData = () => {
 //   if (ev.currentTarget.value !== undefined) {
@@ -12,31 +13,10 @@ function Form() {
   return (
     <div className='section-page__fill  {/*  collapsable-close*/}'>
       <SectionTitle icon='far fa-keyboard' title='Rellena' />
-      <form
-        action=''
-        method='GET'
-        id='info-user'
-        className='section-page__fill__form collapsable-content'
-      >
-        <FormInput
-          htmlFor='fullname'
-          value='Nombre completo'
-          className='input-name'
-          type='text'
-          name='fullname'
-          id='fullname'
-          placeholder='Ej: Sally Jill'
-        />
+      <form action='' method='GET' id='info-user' className='section-page__fill__form collapsable-content'>
+        <FormInput htmlFor='fullname' value='Nombre completo' className='input-name' type='text' name='fullname' id='fullname' placeholder='Ej: Sally Jill' />
 
-        <FormInput
-          htmlFor='job'
-          value='Puesto'
-          className='js-input-job'
-          type='text'
-          name='job'
-          id='job'
-          placeholder='Ej: Front-end unicorn'
-        />
+        <FormInput htmlFor='job' value='Puesto' className='js-input-job' type='text' name='job' id='job' placeholder='Ej: Front-end unicorn' />
 
         <label htmlFor='img' className='label-img'>
           Imagen de perfil
@@ -55,45 +35,13 @@ function Form() {
           <div className='img-user js-preview'></div>
         </div>
 
-        <FormInput
-          htmlFor='email'
-          value='Email'
-          className='js-input-mail'
-          type='email'
-          name='email'
-          id='email'
-          placeholder='Ej: sally-hill@gmail.com'
-        />
+        <FormInput htmlFor='email' value='Email' className='js-input-mail' type='email' name='email' id='email' placeholder='Ej: sally-hill@gmail.com' />
 
-        <FormInput
-          htmlFor='phone'
-          value='Teléfono'
-          className='js-input-phone'
-          type='tel'
-          name='phone'
-          id='phone'
-          placeholder='Ej: 555-55-55-55'
-        />
+        <FormInput htmlFor='phone' value='Teléfono' className='js-input-phone' type='tel' name='phone' id='phone' placeholder='Ej: 555-55-55-55' />
 
-        <FormInput
-          htmlFor='linkendin'
-          value='Linkendin'
-          className='js-input-linkedin'
-          type='text'
-          name='linkendin'
-          id='linkendin'
-          placeholder='Ej: linkendin.com/in/sally.hill@gmail.com'
-        />
+        <FormInput htmlFor='linkendin' value='Linkendin' className='js-input-linkedin' type='text' name='linkendin' id='linkendin' placeholder='Ej: linkendin.com/in/sally.hill@gmail.com' />
 
-        <FormInput
-          htmlFor='github'
-          value='Github'
-          className='js-input-github'
-          type='text'
-          name='github'
-          id='github'
-          placeholder='Ej: @sally-hill'
-        />
+        <FormInput htmlFor='github' value='Github' className='js-input-github' type='text' name='github' id='github' placeholder='Ej: @sally-hill' />
       </form>
     </div>
   );
