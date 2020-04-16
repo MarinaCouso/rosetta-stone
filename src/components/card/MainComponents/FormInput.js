a0f5812d7a512d8113bcae28c5ab2afab9211e11
Martamp <motymont@gmail.com>
2020-04-16 12:01:15 +0200

    hey

--- src/components/card/MainComponents/FormInput.js
+++ src/components/card/MainComponents/FormInput.js
@@ -4,11 +4,7 @@ function FormInput(props) {
   function handleInputForm(ev) {
     // const inputDataLabel = ev.currentTarget.name;
     // console.log(inputDataLabel);
-    console.log(
-      'Me han cambiado',
-      ev.currentTarget.name,
-      ev.currentTarget.value
-    );
+    console.log('Me han cambiado', ev.currentTarget.name, ev.currentTarget.value);
     props.handleInputForm({
       value: ev.currentTarget.value,
       name: ev.currentTarget.name,
@@ -17,16 +13,7 @@ function FormInput(props) {
   return (
     <div>
       <label htmlFor={props.htmlFor}>{props.label}</label>
-      <input
-        className={props.className}
-        type={props.type}
-        name={props.name}
-        id={props.id}
-        placeholder={props.placeholder}
-        onKeyUp={handleInputForm}
-        href={props.href}
-        required
-      />
+      <input className={props.className} type={props.type} name={props.name} id={props.id} placeholder={props.placeholder} onKeyUp={handleInputForm} href={props.href} required />
     </div>
   );
 }
