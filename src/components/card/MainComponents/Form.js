c39ee52f189107c884813fd8cc37284a67b5e09e
MarinaCouso <marina.couso.martinez@gmail.com>
2020-04-17 16:56:08 +0200

    Bug

--- src/components/card/MainComponents/Form.js
+++ src/components/card/MainComponents/Form.js
@@ -13,22 +13,22 @@ import Image from './Image';
 function Form(props) {
   console.log(props);
   return (
-    <div className="section-page__fill collapsable-close">
-      <SectionTitle icon="far fa-keyboard" title="Rellena" />
-      <form action="" method="GET" id="info-user" className="section-page__fill__form collapsable-content">
-        <FormInput htmlFor="fullname" label="Nombre completo" className="input-name" type="text" name="name" id="fullname" placeholder="Ej: Sally Jill" handleInputForm={props.handleInputData} value={props.state.name} />
+    <div className='section-page__fill collapsable-close'>
+      <SectionTitle icon='far fa-keyboard' title='Rellena' />
+      <form action='' method='GET' id='info-user' className='section-page__fill__form collapsable-content'>
+        <FormInput htmlFor='fullname' label='Nombre completo' className='input-name' type='text' name='name' id='fullname' placeholder='Ej: Sally Jill' handleInputData={props.handleInputData} value={props.state.name} />
 
-        <FormInput htmlFor="job" label="Puesto" className="js-input-job" type="text" name="job" id="job" placeholder="Ej: Front-end unicorn" handleInputForm={props.handleInputData} value={props.state.job} />
+        <FormInput htmlFor='job' label='Puesto' className='js-input-job' type='text' name='job' id='job' placeholder='Ej: Front-end unicorn' handleInputForm={props.handleInputData} value={props.state.job} />
 
         <Image handleInputImage={props.handleInputImage} state={props.state} />
 
-        <FormInput htmlFor="email" label="Email" className="js-input-mail" type="email" name="email" id="email" placeholder="Ej: sally-hill@gmail.com" href="" handleInputForm={props.handleInputData} value={props.state.email} />
+        <FormInput htmlFor='email' label='Email' className='js-input-mail' type='email' name='email' id='email' placeholder='Ej: sally-hill@gmail.com' href='' handleInputForm={props.handleInputData} value={props.state.email} />
 
-        <FormInput htmlFor="phone" label="Teléfono" className="js-input-phone" type="tel" name="phone" id="phone" placeholder="Ej: 555-55-55-55" handleInputForm={props.handleInputData} value={props.state.phone} />
+        <FormInput htmlFor='phone' label='Teléfono' className='js-input-phone' type='tel' name='phone' id='phone' placeholder='Ej: 555-55-55-55' handleInputForm={props.handleInputData} value={props.state.phone} />
 
-        <FormInput htmlFor="linkedin" label="Linkedin" className="js-input-linkedin" type="text" name="linkedin" id="linkedin" placeholder="Ej: linkedin.com/in/sally.hill@gmail.com" href="" handleInputForm={props.handleInputData} value={props.state.linkedin} />
+        <FormInput htmlFor='linkedin' label='Linkedin' className='js-input-linkedin' type='text' name='linkedin' id='linkedin' placeholder='Ej: linkedin.com/in/sally.hill@gmail.com' href='' handleInputForm={props.handleInputData} value={props.state.linkedin} />
 
-        <FormInput htmlFor="github" label="Github" className="js-input-github" type="text" name="github" id="github" placeholder="Ej: @sally-hill" href="" handleInputForm={props.handleInputData} value={props.state.github} />
+        <FormInput htmlFor='github' label='Github' className='js-input-github' type='text' name='github' id='github' placeholder='Ej: @sally-hill' href='' handleInputForm={props.handleInputData} value={props.state.github} />
       </form>
     </div>
   );
--- src/components/card/MainComponents/ShareTwitter.js
+++ src/components/card/MainComponents/ShareTwitter.js
@@ -1,96 +1,49 @@
 import React from 'react';
 
 // Prueba con datos fake porque la API necesita recibir todos los datos completos
-const fakeData = {
-  palette: '1',
-  name: 'marina',
-  job: 'cualquiera',
-  photo: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACmUlEQVR4AYyMA8xlRxTHfzNz8fw+r71Bbdu2bRthGTWqGdV2G9eOq2Bt43v2u3fm9NZu9yRHf7ElJbXjHkh6+T9x+n/Nm468ESc3IjJTxo964F8D5JXJO8pb02fy1+r07iKKwbqk5UZZe9iFf6QVgNwzcUeG40/ImZvozd2BMWrq2K/ulm92OxDfJHgKQh8AYgeED+L5z6rJ736nAOT26cspdGeS8aoEgwOdaOqD7z/7BKNzVp2YnbJm5sC2C5ixz+dgNKiknQe+vwJldzJy5dYXAheiBUw/RaYB2WjPSmnnPZtrdxjorJ5NdcVUvPxmiiOrwBmQpB0DaLtQ0wpOoB1AHpiYgoE8frrFPudczy6X3YqbWKFjh8hPL4FvoOGgCVgg4gTtqtmZSQhUQwgE0gaGC6zqH8j3q09ntZ3FuC3ywX1XQ7cH/RgqCroO6XkDXjxe3DHwY1gBbArpHCQ88NTD1LytCYMs2cCR6jlqm4tQb0MnDSUDQ3WoDOBpy6e0ggMRwGrGV6ZYsklgZAO+zpFzPvl+TIFNsNFC00eCLpQVNA3mRnvggel8Y0dEg9MUdZ31ecOS9cNEtottN5F6nRO2e5ZJqoz0AtRAG6lloJn+1NzoDqqBuzD0e+A01EJ2LcxnzN8MrS4T3BrOHn2TbdMLAVCBhcgQrxlBxf5NCmBd8bpPBqetOzDMtFF+BIFNOgbPJW3BSNIOUgoKhm47MavCO6mnXjzJA5DIP2l85aRPsqOlHQsDLXQUobq/BiStf2zBiaa9cAh08TtvlIsAFADA2uxNA07Fb0vYPjBbbBGk+oRhhNKOKFL0uyG9WhEdZT71c3LSwKrHqn8K+EPQhUnQBaLsgaIcoNDioZ15J9nvTmr/sJ4FyOoBUrwhHG0SnxcAAAAASUVORK5CYII=',
-  email: 'marina@madrid.es',
-  phone: '736736721',
-  linkedin: 'https://www.linkedin.com/in/marinacousomartinez/',
-  github: 'https://github.com/MarinaCouso',
-};
-// import '../../../stylesheets/components/_section-design.scss';
-
-// JS COPIADO DEL PROYECTO MODULO 2 EQUIPO 1
-// const shareButtonDiv = document.querySelector('.js-share__button');
-// const button = document.querySelector('.js-button');
-// const divList = document.querySelectorAll('.js-title-collapse');
-// const myCard = document.querySelector('.js-share__card');
-
-// function unCollapse() {
-//   shareButtonDiv.classList.toggle('hidden');
-// }
-// divList[2].addEventListener('click', unCollapse);
-
-// /////////////////
-
-// function createCard() {
-// PTE LIFTING
-// const json = {
-//   palette: getRadioValue(),
-//   name: document.querySelector('.js-form-input-name').value,
-//   job: document.querySelector('.js-form-input-job').value,
-//   tel: document.querySelector('.js-form-input-tel').value,
-//   email: document.querySelector('.js-form-input-email').value,
-//   linkedin: document.querySelector('.js-form-input-linkedin').value,
-//   github: document.querySelector('.js-form-input-github').value,
-//   photo: photo
+// const fakeData = {
+//   palette: '1',
+//   name: 'marina',
+//   job: 'cualquiera',
+//   photo: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACmUlEQVR4AYyMA8xlRxTHfzNz8fw+r71Bbdu2bRthGTWqGdV2G9eOq2Bt43v2u3fm9NZu9yRHf7ElJbXjHkh6+T9x+n/Nm468ESc3IjJTxo964F8D5JXJO8pb02fy1+r07iKKwbqk5UZZe9iFf6QVgNwzcUeG40/ImZvozd2BMWrq2K/ulm92OxDfJHgKQh8AYgeED+L5z6rJ736nAOT26cspdGeS8aoEgwOdaOqD7z/7BKNzVp2YnbJm5sC2C5ixz+dgNKiknQe+vwJldzJy5dYXAheiBUw/RaYB2WjPSmnnPZtrdxjorJ5NdcVUvPxmiiOrwBmQpB0DaLtQ0wpOoB1AHpiYgoE8frrFPudczy6X3YqbWKFjh8hPL4FvoOGgCVgg4gTtqtmZSQhUQwgE0gaGC6zqH8j3q09ntZ3FuC3ywX1XQ7cH/RgqCroO6XkDXjxe3DHwY1gBbArpHCQ88NTD1LytCYMs2cCR6jlqm4tQb0MnDSUDQ3WoDOBpy6e0ggMRwGrGV6ZYsklgZAO+zpFzPvl+TIFNsNFC00eCLpQVNA3mRnvggel8Y0dEg9MUdZ31ecOS9cNEtottN5F6nRO2e5ZJqoz0AtRAG6lloJn+1NzoDqqBuzD0e+A01EJ2LcxnzN8MrS4T3BrOHn2TbdMLAVCBhcgQrxlBxf5NCmBd8bpPBqetOzDMtFF+BIFNOgbPJW3BSNIOUgoKhm47MavCO6mnXjzJA5DIP2l85aRPsqOlHQsDLXQUobq/BiStf2zBiaa9cAh08TtvlIsAFADA2uxNA07Fb0vYPjBbbBGk+oRhhNKOKFL0uyG9WhEdZT71c3LSwKrHqn8K+EPQhUnQBaLsgaIcoNDioZ15J9nvTmr/sJ4FyOoBUrwhHG0SnxcAAAAASUVORK5CYII=',
+//   email: 'marina@madrid.es',
+//   phone: '736736721',
+//   linkedin: 'https://www.linkedin.com/in/marinacousomartinez/',
+//   github: 'https://github.com/MarinaCouso',
 // };
-
-//   console.log(json);
-
-// function showURL(result) {
-//   let cardUrl = result.cardURL;
-//   console.log(cardUrl);
-//   const twitter = document.querySelector('.js-share-button');
-//   const link = document.querySelector('.js-share-link');
-//   link.innerHTML = cardUrl;
-//   link.href = cardUrl;
-//   twitter.href = `https://twitter.com/intent/tweet?text=Mi+tarjeta+se+ha+creado+${cardUrl}`;
-// }
-//   button.classList.remove('share__button__enabled');
-//   button.classList.add('share__button__unabled');
-//   myCard.classList.remove('hidden');
-// }
-
-//
 // F(x) de los apuntes
 let showCard = 'Aún no está creada la tarjeta';
 
-const json = fakeData;
-function showURL(result) {
-  console.log('estoy en ello', result.cardURL);
-  // debugger;
-  if (result.success) {
-    showCard = `<a href='${result.cardURL}'></a>`;
-  } else {
-    showCard = 'ERROR:' + result.error;
-  }
-}
-console.log(JSON.stringify(json));
-function createCard() {
-  fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
-    method: 'POST',
-    body: JSON.stringify(json),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(function (resp) {
-      return resp.json();
-    })
-    .then(function (result) {
-      showURL(result);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
 function ShareTwitter(props) {
-  console.log(props.data.state.photo);
-
+  // console.log(props.data.state.photo);
+  console.log(JSON.stringify(props.state));
+  function createCard(props) {
+    fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
+      method: 'POST',
+      body: JSON.stringify(props.state),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(function (resp) {
+        return resp.json();
+      })
+      .then(function (result) {
+        showURL(result);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  function showURL(result) {
+    console.log('estoy en ello', result.cardURL);
+    // debugger;
+    if (result.success) {
+      showCard = `<a href='${result.cardURL}'></a>`;
+    } else {
+      showCard = 'ERROR:' + result.error;
+    }
+  }
   return (
     <div className='section-page__card-create'>
       {/* <h3 className='section-page__card-create__title'>La tarjeta ha sido creada</h3> */}
