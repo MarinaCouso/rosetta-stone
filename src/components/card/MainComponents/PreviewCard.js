6af5693ffa81263183fc6a5dff13de2d29892481
MarinaCouso <marina.couso.martinez@gmail.com>
2020-04-17 11:24:31 +0200

    Merge dev with reset

--- src/components/card/MainComponents/PreviewCard.js
+++ src/components/card/MainComponents/PreviewCard.js
@@ -3,36 +3,32 @@ import userImage from '../../../images/sample.jpg';
 import PreviewCardIcons from './PreviewCardIcons';
 
 function PreviewCard(props) {
-<<<<<<< HEAD
-  // console.log(props);
-=======
   console.log(props);
 
   const handleResetButton = () => {
     props.handleResetButton();
   };
 
->>>>>>> dev
   return (
     <div className={`showroom palette0${props.state.palette}`}>
-      <button onClick={handleResetButton} className="reset-button">
-        <i className="far fa-trash-alt"></i>
+      <button onClick={handleResetButton} className='reset-button'>
+        <i className='far fa-trash-alt'></i>
         Reset
       </button>
-      <div className="layout">
+      <div className='layout'>
         <div className={`showroom-card__texts palette${props.state.palette}__div `}>
           <h1 className={`layout__title palette${props.state.palette}__layout--title`}>{props.state.name || 'Gatete Frog'}</h1>
-          <p className="layout__text">{props.state.job || 'Front-end developer'}</p>
+          <p className='layout__text'>{props.state.job || 'Front-end developer'}</p>
         </div>
 
-        <img className="photo" src={props.state.photo || userImage} alt="" title="tu foto" />
+        <img className='photo' src={props.state.photo || userImage} alt='' title='tu foto' />
 
         <nav>
-          <ul className="media-icons">
-            <PreviewCardIcons jsclassicon="js-phone-icon" href={`tel:+34${props.state.phone}`} alt="Phone number" title="Call" icon="icon fas fa-mobile-alt" state={props.state.palette} />
-            <PreviewCardIcons jsclassicon="js-mail-icon" href={`mailto:${props.state.email}`} alt="Mail" title="Mail" icon="icon far fa-envelope" state={props.state.palette} />
-            <PreviewCardIcons jsclassicon="js-linkedin-icon" href={`https://www.linkedin.com/in/${props.state.linkedin}`} alt="Linkedin" title="Go to Linkedin" icon="icon fab fa-linkedin-in" target="_blank" state={props.state.palette} />
-            <PreviewCardIcons jsclassicon="js-github-icon" href={`https://github.com/${props.state.github}`} alt="Github" title="Go to Github" icon="icon fab fa-github-alt" target="_blank" state={props.state.palette} />
+          <ul className='media-icons'>
+            <PreviewCardIcons jsclassicon='js-phone-icon' href={`tel:+34${props.state.phone}`} alt='Phone number' title='Call' icon='icon fas fa-mobile-alt' state={props.state.palette} />
+            <PreviewCardIcons jsclassicon='js-mail-icon' href={`mailto:${props.state.email}`} alt='Mail' title='Mail' icon='icon far fa-envelope' state={props.state.palette} />
+            <PreviewCardIcons jsclassicon='js-linkedin-icon' href={`https://www.linkedin.com/in/${props.state.linkedin}`} alt='Linkedin' title='Go to Linkedin' icon='icon fab fa-linkedin-in' target='_blank' state={props.state.palette} />
+            <PreviewCardIcons jsclassicon='js-github-icon' href={`https://github.com/${props.state.github}`} alt='Github' title='Go to Github' icon='icon fab fa-github-alt' target='_blank' state={props.state.palette} />
           </ul>
         </nav>
       </div>
