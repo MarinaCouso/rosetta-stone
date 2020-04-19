5129b5daa458d79c3833ef78805f505e2b57cf0a
BelenCatalan <labdelante@gmail.com>
2020-04-19 12:55:47 +0200

    validation and url twitter share

--- src/components/card/MainComponents/Share.js
+++ src/components/card/MainComponents/Share.js
@@ -3,16 +3,27 @@ import ShareTwitter from './ShareTwitter';
 import SectionTitle from './SectionTitle';
 
 class Share extends React.Component {
+  // function showURL(result) {
+  //   console.log('estoy en ello', result.cardURL);
+  //   // debugger;
+  //   if (result.success) {
+  //     showCard = `<a href='${result.cardURL}'></a>`;
+  //   } else {
+  //     showCard = 'ERROR:' + result.error;
+  //   }
+  // }
+
   render() {
     return (
       <div>
-        <div className='section-page__share collapsable-close'>
-          <SectionTitle icon='fas fa-share-alt' title='Comparte' />
-          <form className='section-page__share__form collapsable-content'>
-            <button type='submit'>crear tarjeta</button>
+        <div className="section-page__share collapsable-close">
+          <SectionTitle icon="fas fa-share-alt" title="Comparte" />
+          <form className="section-page__share__form collapsable-content">
+            <button type="submit">crear tarjeta</button>
+            {/* <p>{showCard}</p> */}
           </form>
         </div>
-        <ShareTwitter />
+        <ShareTwitter cardURL={result.cardURL} />
       </div>
     );
   }
--- src/components/card/MainComponents/ShareTwitter.js
+++ src/components/card/MainComponents/ShareTwitter.js
@@ -1,10 +1,12 @@
 import React from 'react';
 // import '../../../stylesheets/components/_section-design.scss';
-function ShareTwitter() {
+function ShareTwitter(props) {
+  const urlTwitter = 'Tarjeta creada por Rosseta Stone';
+  const hastagTwitter = 'Adalab';
   return (
     <div className="section-page__card-create collapsable-content">
       <h3 className="section-page__card-create__title">La tarjeta ha sido creada</h3>
-      <a href="#" className="section-page__card-create__link-card" target="_blank"></a>
+      <a href={`https://twitter.com/intent/tweet?text=${urlTwitter}&url=${props.cardURL}&hashtags=${hastagTwitter}`} className="section-page__card-create__link-card" target="_blank"></a>
       <button type="submit" className="section-page__card-create__rrss-btn">
         {/* <i className='fab fa-twitter' style='font-size: 18px;'
         ></i>  */}
