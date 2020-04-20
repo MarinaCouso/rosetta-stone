b69fceea0670a661f41958a3c72a427fae830ed7
smgaragorri <smgaragorri@gmail.com>
2020-04-20 12:25:22 +0200

    Update ShareTwitter code  #17

--- src/components/card/MainComponents/ShareTwitter.js
+++ src/components/card/MainComponents/ShareTwitter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ApiData } from '../../../services/ApiData';
 import { checkPropTypes } from 'prop-types';
 let showCard = 'Aún no está creada la tarjeta';
+
 function ShareTwitter(props) {
   console.log(props);
   function createCard() {
@@ -17,15 +18,12 @@ function ShareTwitter(props) {
   //       console.log(error);
   //     });
   // }
-  function showURL(result) {
-    //   console.log('estoy en ello', result.cardURL);
-    //   // debugger;
-    if (result.success) {
-      showCard = `<a href='${result.cardURL}'></a>`;
-    } else {
-      showCard = 'ERROR:' + result.error;
-    }
-  }
+  const showURL = (result) => {
+    console.log('estoy en ello', result.cardURL);
+    return (showCard = result.success
+      ? result.cardURL
+      : 'ERROR:' + result.error);
+  };
 
   const getApiData = () => {
     ApiData(props.data).then((result) => {
@@ -34,18 +32,21 @@ function ShareTwitter(props) {
   };
 
   return (
-    <div className="section-page__card-create">
-      {/* <h3 className='section-page__card-create__title'>La tarjeta ha sido creada</h3> */}
-      {/* <p>Una vez que solicites tu tarjeta en el botón de abajo, espera unos segundos</p>
-      <a href={showCard} className='section-page__card-create__link-card'>
-        {showCard}
-      </a> */}
-      <button type="submit" className="section-page__card-create__rrss-btn" onClick={createCard}>
-        <i className="fab fa-twitter" style={{ fontSize: '18px' }}></i>
+    <div className='section-page__card-create'>
+      <button
+        type='submit'
+        className='section-page__card-create__rrss-btn'
+        onClick={createCard}
+      >
+        <i className='fab fa-twitter' style={{ fontSize: '18px' }}></i>
         Compartir en twitter
       </button>
-      <p>{showCard}</p>
+      {console.log('ESTOY EN EL RETURN' + showCard)}
+      <p>
+        <a href={`${showCard}`}>URL CREADA</a>
+      </p>
     </div>
   );
 }
+
 export default ShareTwitter;
