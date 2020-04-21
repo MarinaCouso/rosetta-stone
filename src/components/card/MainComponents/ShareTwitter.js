1ac73f9c31cd5ca01219fb0487962d3b6377ed60
Martamp <motymont@gmail.com>
2020-04-21 17:55:01 +0200

    CSS update and Twitter button fix

--- src/components/card/MainComponents/ShareTwitter.js
+++ src/components/card/MainComponents/ShareTwitter.js
@@ -5,18 +5,13 @@ function ShareTwitter(props) {
   const urlTwitter = 'Tarjeta creada por Rosseta Stone';
   const hastagTwitter = 'Adalab';
   return (
-    <div className='section-page__card-create collapsable-content'>
-      <h3 className='section-page__card-create__title'>
-        La tarjeta ha sido creada
-      </h3>
+    <div className="section-page__card-create collapsable-content">
+      <h3 className="section-page__card-create__title">La tarjeta ha sido creada</h3>
 
-      <button type='submit' className='section-page__card-create__rrss-btn'>
-        <i className='fab fa-twitter' style={{ fontSize: '18px' }}></i>
-        <a
-          href={`https://twitter.com/intent/tweet?text=${urlTwitter}&url=${props.cardURL}&hashtags=${hastagTwitter}`}
-          className='section-page__card-create__link-card' /*target="_blank"*/
-        >
-          Compartir en twitter
+      <button type="submit" className="section-page__card-create__rrss-btn">
+        <i className="fab fa-twitter" style={{ fontSize: '18px' }}></i>
+        <a href={`https://twitter.com/intent/tweet?text=${urlTwitter}&url=${props.cardURL}&hashtags=${hastagTwitter}`} className="section-page__card-create__link-card" /*target="_blank"*/>
+          Compartir en Twitter
         </a>
       </button>
     </div>
