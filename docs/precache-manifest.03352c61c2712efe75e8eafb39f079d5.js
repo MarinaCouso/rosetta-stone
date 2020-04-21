facc71148e2832e4607b0a041525449d53bfb54e
BelenCatalan <labdelante@gmail.com>
2020-04-21 20:00:50 +0200

    make run docs

--- docs/precache-manifest.03352c61c2712efe75e8eafb39f079d5.js
+++ docs/precache-manifest.03352c61c2712efe75e8eafb39f079d5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6eed8c4ef1331ae63d316ebfe92c8266",
+    "revision": "3b2495507ca8910a04da511c598eb86e",
     "url": "./index.html"
   },
   {
-    "revision": "486e1ecdaa8d01d1370b",
+    "revision": "63ae1d02d67f2131d711",
     "url": "./static/css/main.d4ec65f0.chunk.css"
   },
   {
-    "revision": "1c7d3121396da60fbcf3",
-    "url": "./static/js/2.ff1bae61.chunk.js"
+    "revision": "d06299c52165e585bdfc",
+    "url": "./static/js/2.e441f619.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "./static/js/2.ff1bae61.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.e441f619.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "486e1ecdaa8d01d1370b",
-    "url": "./static/js/main.cdd17019.chunk.js"
+    "revision": "63ae1d02d67f2131d711",
+    "url": "./static/js/main.ab312f8b.chunk.js"
   },
   {
     "revision": "ef0e3c2bbbad658ef01a",
