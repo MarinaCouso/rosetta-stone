d24ac3b5eb7363c861dc1b640b6069866a1f750f
smgaragorri <smgaragorri@gmail.com>
2020-04-21 17:39:41 +0200

    Fix little errors in SASS #21

--- src/components/card/MainComponents/ShareTwitter.js
+++ src/components/card/MainComponents/ShareTwitter.js
@@ -11,14 +11,13 @@ function ShareTwitter(props) {
       </h3>
 
       <button type='submit' className='section-page__card-create__rrss-btn'>
-        <i className='fab fa-twitter' style={{ fontSize: '18px' }}>
-          <a
-            href={`https://twitter.com/intent/tweet?text=${urlTwitter}&url=${props.cardURL}&hashtags=${hastagTwitter}`}
-            className='section-page__card-create__link-card' /*target="_blank"*/
-          >
-            Compartir en twitter
-          </a>
-        </i>
+        <i className='fab fa-twitter' style={{ fontSize: '18px' }}></i>
+        <a
+          href={`https://twitter.com/intent/tweet?text=${urlTwitter}&url=${props.cardURL}&hashtags=${hastagTwitter}`}
+          className='section-page__card-create__link-card' /*target="_blank"*/
+        >
+          Compartir en twitter
+        </a>
       </button>
     </div>
   );
