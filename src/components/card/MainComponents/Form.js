933d5f7f183c69b7406b08e41ca5e93912f2fc01
BelenCatalan <labdelante@gmail.com>
2020-04-21 16:40:39 +0200

    delete incomplet message, made by conditionals in input form #19

--- src/components/card/MainComponents/Form.js
+++ src/components/card/MainComponents/Form.js
@@ -9,7 +9,7 @@ function Form(props) {
       <SectionTitle icon="far fa-keyboard" title="Rellena" />
       <form action="" method="GET" id="info-user" className="section-page__fill__form collapsable-content">
         <FormInput htmlFor="fullname" label="Nombre completo" className="input-name" type="text" name="name" id="fullname" placeholder="Ej: Sally Jill" handleInputData={props.handleInputData} value={props.state.name} />
-        {props.state.name ? '' : <p>Este campo es obligatorio</p>}
+
         <FormInput htmlFor="job" label="Puesto" className="js-input-job" type="text" name="job" id="job" placeholder="Ej: Front-end unicorn" handleInputData={props.handleInputData} value={props.state.job} />
 
         <Image handleInputImage={props.handleInputImage} state={props.state} />
